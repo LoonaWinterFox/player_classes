b65dac0fd54755791fb2d145641d228b8b10b203
LoonaWinterFox <leifriverivy@gmail.com>
2022-03-15 21:15:35 +1300

    .

---
+++ player_plus_weapons.cpp
@@ -0,0 +1,50 @@
+
+#include <iostream>
+
+typedef int Weapon;
+class Player{
+private:
+	float x, y, z = 0.0;
+	float speed;
+	int max_health;
+	int health;
+	bool is_alive = true;
+	int weapon = 5;
+public:
+	float GetHealth(){return health;}
+	float GetX(){return x;}
+	float GetY(){return y;}
+	float GetZ(){return z;}
+
+	void InitializePlayer(){
+		health = max_health;
+		is_alive = true;
+
+	}
+
+	void SetHealth(int new_health){
+		health = new_health;
+		if (health <= 0){
+			is_alive = false;
+		}
+
+	}
+
+	Weapon* GetWeapon(int index){
+		weapon = index
+		return &(weapon);
+	}
+
+	Weapon ReturnWeaponId(){
+		return weapon;
+	}
+
+};
+int main(){
+	Player player;
+	player.SetHealth(100);
+	std::cout << player.GetHealth(), "\n";
+	player.GetWeapon(10);
+	std::cout << player.ReturnWeaponId(), "\n";
+	return 0;
+}
