b773a27ebbc51d12ad511f4d689ff84c22c0dbb8
loona <leifriverivy@gmail.com>
2022-03-15 16:52:29 +1300

    added new player class

---
+++ playerclass.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+class Player{
+private:
+	float x, y, z = 0.0;
+	float speed;
+	int max_health;
+	int health;
+	bool is_alive = true;
+public:
+	float GetHealth(){return health;}
+	float GetX(){return x;}
+	float GetY(){return y;}
+	float GetZ(){return z;}
+
+	void InitializePlayer(){
+		health = max_health;
+		is_alive = true;
+
+	}
+
+	void SetHealth(int new_health){
+		health = new_health;
+		if (health <= 0){
+			is_alive = false;
+		}
+
+	}
+
+
+};
+int main(){
+	Player player;
+	player.SetHealth(100);
+	std::cout << player.GetHealth(), "\n\n";
+	std::cout << player.GetX(), "\n";
+	
+	return 0;
+}
