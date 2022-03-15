c78b2b0ba3ada46276817701e095eeb934c535c2
LoonaWinterFox <leifriverivy@gmail.com>
2022-03-15 23:00:16 +1300

    FOXES!!!!!!

---
+++ fox_class.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+class Fox{
+private:
+	char name[] = "";
+	const bool is_cute = true;
+	int age;
+	bool floof = true;
+
+public:
+	void PetFox(){
+		std::cout << "*Happy Fox Sounds*\n";
+	}
+	
+	bool IsFloofy(){
+		return floof;
+	}
+
+};
+
+int main(){
+	Fox fox[20];
+	for (int i = 0; i < 20, i++){
+		fox[i].PetFox();
+	}
+
+	return 0;
+}
